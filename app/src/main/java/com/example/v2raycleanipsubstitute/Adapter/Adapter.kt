190d28ee4d5bfc7c7634d0ad752c04397a23f3df
merajuni7ed <merajmusavi1243m@gmail.com>
2023-06-02 22:13:08 +0330

    last design of recycler item

--- app/src/main/java/com/example/v2raycleanipsubstitute/Adapter/Adapter.kt
+++ app/src/main/java/com/example/v2raycleanipsubstitute/Adapter/Adapter.kt
@@ -1,23 +1,25 @@
 package com.example.v2raycleanipsubstitute.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.v2raycleanipsubstitute.DataModel.ConfigModel
 import com.example.v2raycleanipsubstitute.databinding.ItemBinding
 
-class Adapter(val con:Context,val configLi:MutableList<ConfigModel>): RecyclerView.Adapter<Adapter.ViewHolder>() {
+class Adapter(val con: Context, val configLi: MutableList<ConfigModel>) :
+    RecyclerView.Adapter<Adapter.ViewHolder>() {
 
-    inner class ViewHolder(val item:ItemBinding): RecyclerView.ViewHolder(item.root) {
+    inner class ViewHolder(val item: ItemBinding) : RecyclerView.ViewHolder(item.root) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(ItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 20
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
